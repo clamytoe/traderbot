77fc5a5247d369f2ee7555a2e99c7c384c74697c
Martin Uribe <clamytoe@gmail.com>
2025-08-04 14:02:02 -0500

    Added TODO steps

--- traderbot/app.py
+++ traderbot/app.py
@@ -5,6 +5,37 @@ app.py
 OGS Trading Bot
 """
 
+# INITIAL CHECKS
+# TODO: Check the position
+# TODO: Check if tradable
+
+# GENERAL TREND ANALYSIS
+# TODO: Load 30-min candles
+# TODO: Perform general trend analysis
+
+# LOOP
+# TODO: Load 5-min candles
+# TODO: Perform instant trend analysis
+# TODO: Perform RSI analysis
+# TODO: Perform stochastic analysis
+
+# SUBMIT ORDER
+# TODO: Submit order
+# TODO: Check position
+
+# "ENTER POSITION" MODE
+# TODO: Check take profit
+# TODO: Check stop loss
+# TODO: Check stochastic crossing
+
+# "EXIT POSITION" MODE
+# TODO: Check order
+# TODO: Check position
+
+# PAUSE
+# TODO: Wait 15-min
+# TODO: Back to beggining
+
 
 def main():
     print("Successfully installed your project file: traderbot")
