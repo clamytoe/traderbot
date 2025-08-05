2bdf4dd594c197184665cae3d26a54be8c9a36c8
Martin Uribe <clamytoe@gmail.com>
2025-08-05 13:10:12 -0500

    Added basic flow logic

--- traderbot/app.py
+++ traderbot/app.py
@@ -1,20 +1,51 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
+
 from traderbot.logger import initialize_logger
 from traderbot.traderbotlib import *
 
+# Initialize the logger
 lg = initialize_logger()
 
+
 # Check trading account status
+def check_account_status():
+    try:
+        pass
+    except Exception as e:
+        lg.error("Could not get account status")
+        lg.info(str(e))
+        sys.exit(1)
+
 
 # Close current orders
+def close_open_orders():
+    open_orders = []
+    lg.info(f"List of open orders: {open_orders}")
 
-# Execute the main trading logic
+    for order in open_orders:
+        # close the order
+        lg.info(f"Closed order: {order.id}")
+
+    lg.info("Closing orders completed.")
+
+
+def get_ticker():
+    ticker = input("Enter ticker symbol: ").strip().upper()
+    lg.info(f"Ticker symbol: {ticker}")
+
+    return ticker
 
 
 def main():
-    lg.info("Successfully installed your project file: traderbot")
+    check_account_status()
+    close_open_orders()
+
+    ticker = get_ticker()
+
+    print(f"Working with ticker: {ticker}")
 
 
 if __name__ == "__main__":
